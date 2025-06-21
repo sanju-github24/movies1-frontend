5ceba8616421f74e88cff4b3ac55353b17b915b4
sanjay <sanjusanjay0444@gmail.com>
2025-06-21 09:59:32 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext.jsx';
-import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <MantineProvider withNormalizeCSS withGlobalStyles>
-        <AppContextProvider>
-          <App />
-        </AppContextProvider>
-      </MantineProvider>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
