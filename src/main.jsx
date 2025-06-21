4026dc445ace64d1443dc032ab4f66286803e1dd
sanjay <sanjusanjay0444@gmail.com>
2025-06-21 23:09:26 +0530

    Fix: Include homepage movies in category filtering

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AppContextProvider>
         <App />
+        <Analytics/>
       </AppContextProvider>
     </BrowserRouter>
   </StrictMode>
