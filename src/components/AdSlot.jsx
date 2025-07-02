2468d4e752dfc2c02e4ba0f9f8b3290c4521a2c7
sanjay <sanjusanjay0444@gmail.com>
2025-07-02 09:50:30 +0530

    Initial commit or your message

--- src/components/AdSlot.jsx
+++ src/components/AdSlot.jsx
@@ -4,14 +4,14 @@ const AdSlot = () => {
   useEffect(() => {
     const adScripts = [
       "https://www.profitableratecpm.com/bhtv863quq?key=54798d6c5809f66d5ff99817e8595b91",
-      "//pl27059556.profitableratecpm.com/49/13/17/4913176845b7ad7495287a13b263e654.js"
+      "https://pl27059556.profitableratecpm.com/49/13/17/4913176845b7ad7495287a13b263e654.js"
     ];
 
     adScripts.forEach(src => {
       const script = document.createElement('script');
       script.src = src;
       script.async = true;
-      script.type = "text/javascript";
+      script.type = "text/javascript"; // <- important
       document.getElementById('ad-slot')?.appendChild(script);
     });
   }, []);
@@ -22,3 +22,4 @@ const AdSlot = () => {
 };
 
 export default AdSlot;
+
