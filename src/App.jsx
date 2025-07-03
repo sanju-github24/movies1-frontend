287f685057361edbf30a4a05fc1c5e77d68efbdd
sanjay <sanjusanjay0444@gmail.com>
2025-07-03 11:22:27 +0530

    Initial commit or your message

--- src/App.jsx
+++ src/App.jsx
@@ -61,57 +61,50 @@ const hideOnlyCategoryBar = hideOnPaths.includes(location.pathname);
       />
 
       
+{/* Navbar + Category */}
+{!hideNavbarAndCategoryBar && <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}
+    {!hideNavbarAndCategoryBar && !hideOnlyCategoryBar && (
+      <>
+        <CategoryBar
+          onCategoryClick={handleCategoryClick}
+          onLanguageClick={handleLanguageClick}
+        />
+      </>
+    )}
 
-      {!hideNavbarAndCategoryBar && (
-  <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-)}
+    {/* Ad scripts - load once */}
+    <PopAdsScript /> {/* ✅ Now this loads only once, outside Routes */}
 
-{!hideNavbarAndCategoryBar && !hideOnlyCategoryBar && (
-  <>
-  <CategoryBar
-    onCategoryClick={handleCategoryClick}
-    onLanguageClick={handleLanguageClick}
-  />
-   <PopAdsScript/>
-  </>
-)}
+    {/* Routes */}
+    <Routes>
+      <Route path="/" element={<Home searchTerm={searchTerm} />} />
+      <Route path="/movie/:code" element={<MovieDetail />} />
+      <Route path="/search" element={<SearchResults />} />
+      <Route path="/category/:name" element={<CategoryPage />} />
+      <Route path="/latest" element={<LatestUploads />} />
+      <Route path="/blogs" element={<BlogList />} />
+      <Route path="/blogs/:slug" element={<BlogViewer />} />
 
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home searchTerm={searchTerm} />} />
-        <Route path="/movie/:code" element={<MovieDetail />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/category/:name" element={<CategoryPage />} />
-        <Route path="/latest" element={<LatestUploads />} />
-        <Route path="/blogs" element={<BlogList />} />
-  <Route path="/blogs/:slug" element={<BlogViewer />} />
-  <PopAdsScript/>
+      <Route path="/admin" element={<ProtectedAdminRoute />}>
+        <Route index element={<Navigate to="/admin/upload" />} />
+        <Route path="upload" element={<AdminUpload />} />
+        <Route path="blog-editor" element={<BlogEditor />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="stories" element={<AdminStories />} />
+      </Route>
 
-        <Route path="/admin" element={<ProtectedAdminRoute />}>
-  <Route index element={<Navigate to="/admin/upload" />} />
-  <Route path="upload" element={<AdminUpload />} />
-  <Route path="blog-editor" element={<BlogEditor />} />
-  <Route path="dashboard" element={<AdminDashboard />} />
-  <Route path="/admin/stories" element={<AdminStories />} />
+      {!isLoggedIn && (
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="/email-verify" element={<EmailVerify />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </>
+      )}
 
-</Route>
-
-
-        {/* Auth Pages */}
-        {!isLoggedIn && (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/email-verify" element={<EmailVerify />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <PopAdsScript/>
-          </>
-        )}
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
-  );
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </div>
+);
 };
 
 export default App;
